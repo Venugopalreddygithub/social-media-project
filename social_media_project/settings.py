0382801e44416a98bc949dc2ccab08f4a68048f1
venugopal <bvenugopalreddy16@gmail.com>
2024-01-07 21:13:20 +0530

    aws settings.py changed

--- social_media_project/settings.py
+++ social_media_project/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import environ
+# import environ
 import os
 
-env = environ.Env()
+# env = environ.Env()
 
 # # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,14 +81,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'social_media_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases 
+import os 
 
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
